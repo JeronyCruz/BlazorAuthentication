1a8d1de985e4950e5cd9b472d7a831335623cb03
Jerony Ulises Cruz Fermin <jerony_cruz@ucne.edu.do>
2024-11-07 16:53:29 -0400

    Add project files.

---
+++ BlazorAuthentication/DAL/Context.cs
@@ -0,0 +1,22 @@
+﻿using BlazorAuthentication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorAuthentication.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+        public DbSet<UserAccount> UserAccounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<UserAccount>().HasData(new List<UserAccount>()
+            { 
+                new UserAccount() {Id = 1, UserName = "Jerony", Password = "Venom01", Role = "Admin"},
+                new UserAccount() {Id = 2, UserName = "Christopher", Password = "Carnage02", Role = "User"}
+                });
+        }
+    }
+}
---
+++ BlazorAuthentication/Models/ViewModels/LoginViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAuthentication.Models.ViewModels
+{
+    public class LoginViewModels
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor ingrese el nombre de usuario")]
+        public string? Username { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage ="Por favor ingrese su Password")]
+        public string? Password { get; set; }
+
+    }
+}
