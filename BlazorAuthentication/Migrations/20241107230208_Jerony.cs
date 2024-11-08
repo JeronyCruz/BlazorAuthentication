c089d438383c01ca524510977e8fc8808197a633
Jerony Ulises Cruz Fermin <jerony_cruz@ucne.edu.do>
2024-11-07 20:24:51 -0400

    Nueva migracion

---
+++ BlazorAuthentication/Migrations/20241107230208_Jerony.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BlazorAuthentication.Migrations
+{
+    /// <inheritdoc />
+    public partial class Jerony : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAccounts", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "UserAccounts",
+                columns: new[] { "Id", "Password", "Role", "UserName" },
+                values: new object[,]
+                {
+                    { 1, "Venom01", "Admin", "Jerony" },
+                    { 2, "Carnage02", "User", "Christopher" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserAccounts");
+        }
+    }
+}
